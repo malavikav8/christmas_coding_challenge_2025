1ab20d383d3ac909470d6ceeeee38764b68d719c
malavikav8 <75568886+malavikav8@users.noreply.github.com>
2025-12-14 22:52:50 +0000

    day11.sql

--- day11.sql
+++ day11.sql
@@ -1,10 +1,23 @@
-/* Question: https://leetcode.com/problems/article-views-i/description/ */
+--https://leetcode.com/problems/queries-quality-and-percentage/
 -- Write your PostgreSQL query statement below
-SELECT DISTINCT author_id as id FROM Views
-WHERE author_id = viewer_id
-ORDER BY author_id asc;
+select query_name, 
+ROUND(AVG(rating::numeric / position), 2) AS quality,
+ROUND(AVG(CASE WHEN rating < 3 THEN 100.0 ELSE 0 END), 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name
 
-/* Question: https://leetcode.com/problems/invalid-tweets/ */
--- Write your PostgreSQL query statement below
-SELECT tweet_id FROM tweets
-WHERE  CHAR_LENGTH(content) > 15 ;
+/* IN PANDAS
+import pandas as pd
+
+def queries_stats(queries: pd.DataFrame) -> pd.DataFrame:
+   queries['quality'] = queries['rating'] / queries['position']
+   queries['poor_query_percentage'] = (queries['rating'] < 3) * 100
+   result = queries.groupby('query_name')[['quality','poor_query_percentage']].mean().reset_index()
+   result[['quality', 'poor_query_percentage']] = (result[['quality', 'poor_query_percentage']] + 1e-9).round(2) 
+
+   return result
+   */
+   
+
+    
+
